455ad9fb8176541b3ce53c953a6ee7b9113fc5e3
le-Q <32103046+le-Q@users.noreply.github.com>
2019-10-29 11:47:00 +0100

    Add test?

---
+++ test/Main.java
@@ -0,0 +1,22 @@
+import no.ticket.MainJavaFX;
+import no.ticket.Model.Manager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Main {
+    static Manager manager;
+    @BeforeAll
+    public static void init(){
+        manager = new Manager("", "Test", 123456, LocalDate.of(1990,2,2), "test", 999);
+    }
+    @Test //Denne testen viser at du kan hente en liste med Personer og bruke deres ID for å logge inn
+    void loginTest() {
+        MainJavaFX.setCurrentPerson(manager, true);
+        assertEquals(MainJavaFX.getCurrentUser().getId(), manager.getId());
+    }
+
+}
