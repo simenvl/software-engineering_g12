976f93aa3f55a07a941c26772ea88bbfb9e0937c
le-Q <32103046+le-Q@users.noreply.github.com>
2019-11-12 12:06:17 +0100

    Revert "fixed cancel button"
    This reverts commit 354f5cf8a3451fc91971fda7c8fc9c9f7217b3d2.

--- src/no/ticket/Controller/EventController.java
+++ src/no/ticket/Controller/EventController.java
@@ -49,7 +49,7 @@ public class EventController {
     private Boolean editNewEvent = false;
 
     ObservableList<User> participantsList = FXCollections.observableArrayList();
-    ArrayList<Event> arrayList = DataHandler.getEventList();
+    ArrayList<Event> arrayList = new ArrayList<>();
 
     @FXML
     public void initialize() {
@@ -96,6 +96,7 @@ public class EventController {
         int capacity = Integer.parseInt(txtCapacity.getText());
         LocalDate date = datePicker.getValue();
         String place = txtLocation.getText();
+        arrayList = DataHandler.getEventList();
 
         int agerestrict = 0, time = 0, price = 0;
 
@@ -134,7 +135,7 @@ public class EventController {
     }
 
     public void btnCloseEvent() {
-        //WriteJson.addToJson(arrayList);
+        WriteJson.addToJson(arrayList);
         MainJavaFX.getInstance().setHovedLayout();
     }
 
@@ -164,6 +165,7 @@ public class EventController {
 
 
     public void btnSaveParticipants(ActionEvent actionEvent) {
+
         String name = txtName.getText();
         String rankTitle = txtRankTitle.getText();
         int rank = Integer.parseInt(txtRankNumber.getText());
@@ -180,6 +182,7 @@ public class EventController {
         }
         participantsList.sort(User::compareTo);
         ParticipantsListView.refresh();
+        WriteJson.addToJson(arrayList);
     }
 
 
