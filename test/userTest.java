e04592c61cc43c0d6bf4269047383d47cc10781f
le-Q <32103046+le-Q@users.noreply.github.com>
2019-11-12 12:04:12 +0100

    test

---
+++ test/userTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class userTest {
+    @Test
+    void GetTitleMovie() {
+        //Assertions.assertEquals("Run", event.getTitle());
+    }
+}
