0cb3371dc51af36c4ee800b46bc041b9dadba733
le-Q <32103046+le-Q@users.noreply.github.com>
2019-11-12 15:18:25 +0100

    11 => 9.0.4

--- test/loginTest.java
+++ test/loginTest.java
@@ -15,6 +15,7 @@ public class loginTest {
     public static void init(){
         manager = new Manager("", "Test", 123456, LocalDate.of(1990,2,2), "test", 999);
     }
+
     @Test // Denne testen viser at du kan hente en liste med Personer og bruke deres ID for å logge inn
     void loginTest() {
         //manager.setId(111111); // Setter feil ID, testen vil da feile da ingen bruker blir lagt til og vi får nullpointexception.
@@ -22,6 +23,7 @@ public class loginTest {
             if (Integer.valueOf(manager.getId()).equals(eachAdmin.getId())){
                 MainJavaFX.setCurrentPerson(manager, true);
             }
+
         assertEquals(MainJavaFX.getCurrentUser().getId(), manager.getId());
     }
 
