50b555e7f2903cffe446a0585da0f0238fcbe502
le-Q <quang.d.le@hiof.no>
2019-11-20 16:07:37 +0100

    guest can buy tickets now

--- src/main/java/gruppe/TicketController.java
+++ src/main/java/gruppe/TicketController.java
@@ -1,5 +1,6 @@
 package gruppe;
 
+import gruppe.Model.Person;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -79,24 +80,57 @@ public class TicketController {
     }
 
     public void btnBuyEvent(ActionEvent actionEvent) throws IOException {
-        if (Period.between(MainJavaFX.getCurrentUser().getBirthDate(), LocalDate.now()).getYears() < buyTicketEvent.getAgeRestrict()) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("You are not old enough for this event");
-            alert.showAndWait();
-            System.out.println("You are not old enough for this event");
-        } else {
-            System.out.println("Ticket bought");
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("A confirmation mail has been sendt to: " + MainJavaFX.getCurrentUser().getEmail());
-            alert.showAndWait();
-            Ticket newTicket = new Ticket (buyTicketEvent, MainJavaFX.getCurrentUser());
-            User newUser = new User(MainJavaFX.getCurrentUser(), participantNumber);
-            buyTicketEvent.addParticipants(newUser);
-            System.out.println(newTicket);
-            ArrayList<Event> arrayList = DataHandler.getEventList();
-            WriteJson.addToJson(arrayList);
-            MainJavaFX.setRoot("HovedLayout");
+            if (MainJavaFX.getCurrentUser() == null) {
+                if (txtName.getText().isEmpty() || txtEmail.getText().isEmpty() || txtMobile.getText().isEmpty() || datePicker.getValue() == null) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("You need to fill all the correct information");
+                    alert.showAndWait();
+                } else if (Period.between(datePicker.getValue(), LocalDate.now()).getYears() < buyTicketEvent.getAgeRestrict()) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("You are not old enough for this event");
+                    alert.showAndWait();
+                } else {
+                    try {
+                        int phone = Integer.parseInt(txtMobile.getText());
 
-        }
+                        System.out.println("Ticket bought");
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setContentText("A confirmation mail has been sendt to: " + txtEmail.getText());
+                        alert.showAndWait();
+
+                        Person newPerson = new Person(txtName.getText(), datePicker.getValue(), txtEmail.getText(), phone);
+                        Ticket newTicket = new Ticket(buyTicketEvent, newPerson);
+                        User newUser = new User(newPerson, participantNumber);
+                        buyTicketEvent.addParticipants(newUser);
+                        System.out.println(newTicket);
+                        ArrayList<Event> arrayList = DataHandler.getEventList();
+                        WriteJson.addToJson(arrayList);
+                        MainJavaFX.setRoot("HovedLayout");
+                    } catch (NumberFormatException ex) {
+                        Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                        alert2.setContentText("You phone number must be only numbers");
+                        alert2.showAndWait();
+                    }
+
+                }
+            } else if (Period.between(MainJavaFX.getCurrentUser().getBirthDate(), LocalDate.now()).getYears() < buyTicketEvent.getAgeRestrict()) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("You are not old enough for this event");
+                alert.showAndWait();
+                System.out.println("You are not old enough for this event");
+            } else {
+                System.out.println("Ticket bought");
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("A confirmation mail has been sendt to: " + MainJavaFX.getCurrentUser().getEmail());
+                alert.showAndWait();
+                Ticket newTicket = new Ticket(buyTicketEvent, MainJavaFX.getCurrentUser());
+                User newUser = new User(MainJavaFX.getCurrentUser(), participantNumber);
+                buyTicketEvent.addParticipants(newUser);
+                System.out.println(newTicket);
+                ArrayList<Event> arrayList = DataHandler.getEventList();
+                WriteJson.addToJson(arrayList);
+                MainJavaFX.setRoot("HovedLayout");
+
+            }
     }
 }
